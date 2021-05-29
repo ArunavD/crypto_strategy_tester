5eb22d396bc30f5460db854c99c040a48a9855d4
ArunavD <arunavdas15@gmail.com>
2021-05-29 12:06:18 +0530

    cerebro added

--- tester.py
+++ tester.py
@@ -1,1 +1,14 @@
-import pandas
+import backtrader as bt
+
+
+cerebro = bt.Cerebro()
+
+
+#setting the cash
+cerebro.broker.set_cash(1000000)
+
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+cerebro.run()
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
